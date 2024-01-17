2c58dfdba93c408c7383aff416e35f0f17ec9fe6
ankushcs <ankushcswork@gmail.com>
2024-01-17 21:47:30 +0530

    changes for contact & about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,7 @@
 import "./About.scss";
+import Lottie from "lottie-react";
 import Navbar from "../Navbar/Navbar";
+import aboutMeLottie from "../../assets/aboutUsLottie.json";
 
 const About = () => {
   return (
@@ -8,7 +10,35 @@ const About = () => {
       <div className="about-box-container"></div>
       <div className="about-main-container">
         <Navbar />
-        <div className="about-inner-container"></div>
+        <div className="about-inner-container">
+          <div className="about-details-container">
+            <p className="about-heading">About</p>
+            <p className="about-details-para">
+              Welcome to my portfolio! I am a dedicated frontend developer with
+              over 2 years of hands-on experience in crafting seamless and
+              visually appealing digital experiences. My skill set spans HTML,
+              CSS, JavaScript, Bootstrap, React JS, hooks, and Material-UI
+              (MUI). From building applications from the ground up to enhancing
+              existing projects, I leverage my strong frontend skills to deliver
+              top-notch code that exceeds client expectations. I am fueled by a
+              passion for continuous learning, always seeking to improve and
+              enhance productivity. Each project I undertake becomes an
+              opportunity for growth, pushing me to explore novel approaches and
+              overcome challenges efficiently. My commitment to delivering
+              high-quality code is unwavering, ensuring client satisfaction and
+              contributing to the success of every venture. Beyond technical
+              proficiency, I prioritize communication, collaboration, and
+              teamwork. I believe that these aspects are essential for the
+              seamless execution of projects. With a dynamic approach and
+              dedication, I am ready to contribute impactful digital experiences
+              and exceptional outcomes to new ventures. Let's collaborate and
+              create something extraordinary together!
+            </p>
+          </div>
+          <div className="about-img-container">
+            <Lottie className="lottie" animationData={aboutMeLottie} />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,14 +1,89 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import Navbar from "../Navbar/Navbar";
+import Lottie from "lottie-react";
+import emailLottie from "../../assets/emailLottie.json";
 import "./Contact.scss";
 
 const Contact = () => {
+  const form = useRef();
+  const [messageStatus, setMessageStatus] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_6tj1qul",
+        "template_zqz52c2",
+        form.current,
+        "X1KDNx9ThEYzeiqZx"
+      )
+      .then(
+        (result) => {
+          console.log(result);
+          setMessageStatus(true);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-orange-container"></div>
       <div className="contact-box-container"></div>
       <div className="contact-main-container">
         <Navbar />
-        <div className="contact-inner-container"></div>
+        <div id="contact" className="contact-inner-container ">
+          {/* <h1 className="contact-us-heading">CONTACT US</h1> */}
+          <div className="contact-box">
+            <form ref={form} onSubmit={sendEmail} className="form-container">
+              <div className="label-input-box">
+                <label className="label-field" htmlFor="from_name">
+                  Name
+                </label>
+                <input
+                  required
+                  placeholder="Elon Musk"
+                  className="input-field"
+                  type="text"
+                  name="from_name"
+                />
+              </div>
+              <div className="label-input-box">
+                <label className="label-field" htmlFor="from_email">
+                  Email
+                </label>
+                <input
+                  className="input-field"
+                  type="email"
+                  name="from_email"
+                  placeholder="elonmusk@example.com"
+                  required
+                />
+              </div>
+              <div className="label-input-box">
+                <label className="label-field" htmlFor="message">
+                  Message
+                </label>
+                <textarea
+                  required
+                  placeholder="Hi Ankush, I'd love to talk to you about this project we're working on ..."
+                  className="textarea-field"
+                  name="message"
+                />
+              </div>
+              <button className="send-button" type="submit">
+                Send message
+              </button>
+            </form>
+            <div className="lottie-box">
+              <Lottie className="lottie" animationData={emailLottie} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
